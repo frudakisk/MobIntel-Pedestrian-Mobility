5f1783ad5c4175b2cfd662b0814a21794b83efa8
kfruda <52004052+BeeAdmin12@users.noreply.github.com>
2023-08-24 14:04:52 -0400

    Updated comments for ExploritoryAnalysisLib.py
    Updated the comments for functions in the ExploritoryAnalysisLib.py file to be understandable and complete

--- pythonFiles/Functionality/ExploritoryAnalysisLib.py
+++ pythonFiles/Functionality/ExploritoryAnalysisLib.py
@@ -1,3 +1,10 @@
+"""
+This file contains functions that were used to explore the data in the mobintel
+sensor files as well as the parking lot experiment data (sampled_source_data.csv).
+In this file, we can find a lot of plots and data separation. Any future functions
+that deal with anaylzing files of data should go here.
+"""
+
 import sys, matplotlib.pyplot as plt, pandas as pd, numpy as np
 
 sys.path.append("pythonFiles")
@@ -6,6 +13,17 @@ from Functionality import MLLib as mll, DataFiltrationLib as dfl
 
 #requires the groupby dataframe and the date to look for
 def createLineGraph(df, date, dayByDay=False):
+    """
+    df: a df that contains a subset of data by date from dateAggDf or HourAggDf
+    date: a timestamp object 
+    dayByDay: If False, we focus on creating one plot that has all hours of each day
+    in the given data frame. If True, we focus on each day and look at all hours of 
+    each day separately
+    Returns: A matplotlib graph object
+    Description: This function requires a a dataframe made from the groupBy functions
+    of either dateAggDf or HourAggDf. This function is a helper function for 
+    plotInDepthWeek or plotDays
+    """
     plt.plot(np.array(df.index), np.array(df["sensorid"].values))
     plt.xlabel("Day")
     plt.ylabel("Count of Devices")
@@ -18,7 +36,10 @@ def createLineGraph(df, date, dayByDay=False):
 
 def distanceCounter(dataframe):
     """
-    For each rssi value, capture the count of the distances
+    dataframe: I'm not sure what kind of df goes in here, but it should have a
+    distance column.
+    Returns: a dictionary that keeps track of how many times a distance occured
+    Description: For each rssi value, capture the count of the distances
     """
     dict = {}
     for distance in dataframe["Distance"]:
@@ -31,7 +52,9 @@ def distanceCounter(dataframe):
 def rssiSet():
     """"
     returns: all the unique rssi values from the parking lot experiment
-    get all the rssi values in a set
+    Description: get all the unique rssi values in a set. This function focuses on the 
+    parking lot experiment data (sampled_source_data.csv) and collects all the 
+    unique rssi values in a set
     """
     s1 = mll.sensorDistance("s1")
     s2 = mll.sensorDistance("s2")
@@ -49,6 +72,8 @@ def dateAggDf(filename):
     """
     filename: a mobintel sensor parquet file
     returns: a dataframe where we aggregate it by the date
+    Description: a dataframe where the data is aggregated by days, which comes from
+    the probingtime column in the sensor data files.
     """
     df = dfl.read_file(filename) #read and remove useless columns
     df = dfl.sensor_trim(df) #filters out isphysical == 1
@@ -62,6 +87,8 @@ def hourAggDf(filename):
     """
     filename: a mobintel sensor parquet file
     returns: a dataframe where we aggregate it by the hour
+    Description: a dataframe is created where the data is aggregated by the hour of
+    each day, which comes from the probingtime column in the sensor data files.
     """
     df = dfl.read_file(filename) #read and remove useless columns
     df = dfl.sensor_trim(df) #filters out isphysical == 1
@@ -73,6 +100,13 @@ def hourAggDf(filename):
 
 
 def plotDateAgg(dateAgg):
+    """
+    dateAgg: sensor data that was turned into a data frame that was aggregated
+    by days
+    Returns: a plot of the data with x-axis as the day and y-axis as a count
+    Description: Visualizes the data when it is aggregated by days. We are counting
+    the number of probe request that happen on each day for this week of data.
+    """
     #This block presents the entire week in a general sense
     #Create a line graph of dates
     plt.figure(figsize=(10, 4.8))
@@ -86,7 +120,11 @@ def plotDateAgg(dateAgg):
 def plotInDepthWeek(dateAgg, hourAgg):
     """
     dateAgg: use dateAggDf to make this variable
-    hourAgg: use hourAggDf tp make this variable
+    hourAgg: use hourAggDf to make this variable
+    Returns: a plot that shows the data by the hour but shows the entire week
+    Description: The returned plot uses is a more detailed version of plotDateAgg 
+    because we are not generalizing the days and are focusing in on the hours within
+    the days
     """
     #This block presents one graph of the entire week
     plt.figure(figsize=(15, 4.8))
@@ -101,7 +139,10 @@ def plotDays(dateAgg, hourAgg):
     """
     dateAgg: use dateAggDf to make this variable
     hourAgg: use hourAggDf tp make this variable
-    This block presents each day of the week in separate graphs
+    Returns: several graphs that focus on the hours of just one day each.
+    Description: This block presents each day of the week in separate graphs. We 
+    plot out one day out of the week and show the probe request count per hour on
+    that day. Then we make a new plot for the next day and so on until we are done.
     """
     for timestamp in dateAgg.index:
         date = timestamp.date()
